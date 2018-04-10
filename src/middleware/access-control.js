649d7548619ea6975c560ca4ef09de108fa08168
elias-garcia <eliasgarciadev@gmail.com>
2018-04-10 20:24:15 +0200

    Fixed access control header

--- src/middleware/access-control.js
+++ src/middleware/access-control.js
@@ -3,7 +3,7 @@ const accessControl = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
   }
   if (process.env.NODE_ENV === 'production') {
-    res.header('Access-Control-Allow-Origin', 'https://sporter-api.herokuapp.com/');
+    res.header('Access-Control-Allow-Origin', 'https://sporter-client.herokuapp.com');
   }
   res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
